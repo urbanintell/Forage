365a143ff08f7170a3addfbefa453114bbc76df1
Luseniik <lkromah@aggies.ncat.edu>
2016-06-02 22:56:08 -0700

    Initial Commit

---
+++ Forage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Forage
+//
+//  Created by Lusenii Kromah on 6/2/16.
+//  Copyright © 2016 Derivative. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
